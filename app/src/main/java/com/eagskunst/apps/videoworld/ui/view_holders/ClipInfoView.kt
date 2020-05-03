1d8e3a7f7c18acedb4161eaaf9f9060a5e070ffc
Emmanuel Guerra <emmanuelnypd@gmail.com>
2020-05-03 16:57:56 -0400

    Changing clip info with clipinfoview to bind clip lists

---
+++ app/src/main/java/com/eagskunst/apps/videoworld/ui/view_holders/ClipInfoView.kt
@@ -0,0 +1,7 @@
+package com.eagskunst.apps.videoworld.ui.view_holders
+
+/**
+ * Created by eagskunst in 3/5/2020.
+ */
+class ClipInfoView_ {
+}
---
+++ app/src/main/java/com/eagskunst/apps/videoworld/utils/DownloadState.kt
@@ -0,0 +1,7 @@
+package com.eagskunst.apps.videoworld.utils
+
+/**
+ * Created by eagskunst in 3/5/2020.
+ */
+object DownloadState {
+}
